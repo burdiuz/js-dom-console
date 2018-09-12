d1a830fdd47e29f30560d07aa2fda0b29c9402eb
Oleg Galaburda <burdiuz@gmail.com>
2018-09-12 11:49:38 +0300

    fix quoted strings

--- console.js
+++ console.js
@@ -414,6 +414,12 @@
 
   const buildContent = (content, item) => {
     content.forEach(value => {
+      if (typeof value === 'string') {
+        // shortcut for log strings to not wrap them with quotes
+        item.appendChild(createSimpleValue(value));
+        return;
+      }
+
       const result = convert(value);
 
       if (typeof result === 'object') {
